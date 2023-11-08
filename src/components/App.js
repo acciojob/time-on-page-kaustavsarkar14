50b0b5aa9c12d978955381627fb1e43cf5b64d33
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-08 21:09:13 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [time, setTime] = useState(0)
+  setInterval(() => {
+    setTime(time+1)
+  }, 1000);
   return (
     <div>
-        {/* Do not remove the main div */}
+        You've been on this page for {time} seconds.
     </div>
   )
 }
